17ba58b86b37d8b94fa54d369cfdeb4cf6617c65
pmaen <61318119+pmaen@users.noreply.github.com>
2021-05-23 10:53:09 +0200

    Update speech_recognition.py
    Changed to google voice recognition and added a few tricks to use it free.

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,36 +1,48 @@
-from vosk import Model, KaldiRecognizer
-import sys
-import json
-import os
 import os.path
+import speech_recognition as sr 
 import moviepy.editor as mp
+from pydub import AudioSegment 
+from pydub.utils import make_chunks
+import time
+import glob
 
+start_time= time.time()
+filename = input("Bitte geben Sie den gesamten Dateinamen inklusive Endung an:\n")
+fileaudio = filename + ".wav"
+title = input("Um welches Thema handelt es sich?\n")
 
-model = Model("/home/paul/voskmodel")
-
-rec = KaldiRecognizer(model, 16000)
-
-filename = input("Bitte geben Sie den gesamten Dateinamen inklusive Endung an.\n")
-fileaudio = filename + ".mp3"
-clip = mp.VideoFileClip(filename)
+clip = mp.VideoFileClip(filename) 
 clip.audio.write_audiofile(fileaudio)
 
-f = open(fileaudio + "_transcript.txt","w+")
-wf = open(fileaudio, "rb")
-wf.read(44) # skip header
+myaudio = AudioSegment.from_file(fileaudio, "wav") 
+chunk_length_ms = 60000 # pydub calculates in millisec 
+chunks = make_chunks(myaudio,chunk_length_ms) 
 
-count=1
-while True:
-    data = wf.read(4000)
-    count += 1
-    print(count)
-    if len(data) == 0:
-        break
-    if rec.AcceptWaveform(data):
-        res = json.loads(rec.Result())
-        print (res['text'], file=f)
+r = sr.Recognizer()
+for i, chunk in enumerate(chunks):
+    counterfile = open("counter.txt",'w')
+    chunk_name = "{0}.wav".format(i) 
+    print ("exporting", chunk_name) 
+    chunk.export(chunk_name, format="wav")
 
-res = json.loads(rec.FinalResult())
-print (res['text'], file =f)
-f.close ()
-print ("DONE")
+    audio = sr.AudioFile(chunk_name)
+    with audio as source:
+      audio_file = r.record(source)
+    result = r.recognize_google(audio_file, language='de-DE')
+    with open(chunk_name + ".rectext" ,mode ='w') as file:  
+       file.write(result) 
+    print("Part" + str(i) + " finished.")
+file_pattern = re.compile(r'.*?(\d+).*?')
+def get_order(file):
+    match = file_pattern.match(Path(file).name)
+    if not match:
+        return math.inf
+    return int(match.groups()[0])
+read_files = sorted(glob.glob("*.rectext"), key=get_order)
+
+with open(filename + "_transcript.txt", "w") as outfile:
+    for f in read_files:
+        with open(f, "r") as infile:
+            outfile.write(infile.read())
+            outfile.write("\n")       
+print("Done after %.2f seconds."% (time.time() - start_time))
