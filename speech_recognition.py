6707a2a0094f660a7b16beeaf5db1177e186fd3d
pmaen <61318119+pmaen@users.noreply.github.com>
2021-05-31 10:28:07 +0200

    Update speech_recognition.py

--- speech_recognition.py
+++ speech_recognition.py
@@ -1,41 +1,49 @@
 import os.path
-import speech_recognition as sr 
+import speech_recognition as sr
 import moviepy.editor as mp
-from pydub import AudioSegment 
+from pydub import AudioSegment
 from pydub.utils import make_chunks
 import time
 import glob
-import re 
+import re
 import math
 from pathlib import Path
+import soundfile as sf
 
-start_time= time.time()
-lang = input("Please choose the language for voice recognition by language code.\n deutsch: de-DE")
+lang = input("Please choose the language for voice recognition by language code. (deutsch: de-DE)\n")
 filename = input("Please enter the whole file path including the extension:\n")
 fileaudio = filename + ".wav"
-title = input("Whats the topic?\n")
+title = input("What's the topic?\n")
 
-clip = mp.VideoFileClip(filename) 
+start_time= time.time()
+
+clip = mp.VideoFileClip(filename)
 clip.audio.write_audiofile(fileaudio)
 
-myaudio = AudioSegment.from_file(fileaudio, "wav") 
-chunk_length_ms = 60000 # pydub calculates in millisec 
-chunks = make_chunks(myaudio,chunk_length_ms) 
+myaudio = AudioSegment.from_file(fileaudio, "wav")
+chunk_length_ms = 60000 # pydub calculates in millisec
+chunks = make_chunks(myaudio,chunk_length_ms)
 
 r = sr.Recognizer()
 for i, chunk in enumerate(chunks):
-    counterfile = open("counter.txt",'w')
-    chunk_name = "{0}.wav".format(i) 
-    print ("exporting", chunk_name) 
+    chunk_name = "{0}.wav".format(i)
+    print ("exporting", chunk_name)
     chunk.export(chunk_name, format="wav")
 
     audio = sr.AudioFile(chunk_name)
-    with audio as source:
-      audio_file = r.record(source)
-    result = r.recognize_google(audio_file, language=lang)
-    with open(chunk_name + ".rectext" ,mode ='w') as file:  
-       file.write(result) 
-    print("Part" + str(i) + " finished.")
+    x, fs = sf.read(chunk_name)
+    vol_rms =  x.max() - x.min()
+    if vol_rms <= 6.103515625e-05:
+        os.remove(chunk_name)
+        print(chunk_name + "was empty and therefore deleted.")
+    else:
+        with audio as source:
+            audio_file = r.record(source)
+        result = r.recognize_google(audio_file, language=lang)
+        with open(chunk_name + ".rectext" ,mode ='w') as file:
+            file.write(result)
+        print("Part " + str(i) + " finished.")
+        os.remove(chunk_name)
 file_pattern = re.compile(r'.*?(\d+).*?')
 def get_order(file):
     match = file_pattern.match(Path(file).name)
@@ -48,5 +56,8 @@ with open(filename + "_transcript.txt", "w") as outfile:
     for f in read_files:
         with open(f, "r") as infile:
             outfile.write(infile.read())
-            outfile.write("\n")       
+            outfile.write("\n")
+cleanup = glob.glob("*.rectext")
+for rectextfile in cleanup:
+    os.remove(rectextfile)
 print("Done after %.2f seconds."% (time.time() - start_time))
